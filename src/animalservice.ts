4073e51bcb890915bbf4bec250b9248aa11beedb
Karega K. Scott <kkscott@outlook.com>
2020-07-15 11:36:22 -0500

    Use inline router function

--- src/animalservice.ts
+++ src/animalservice.ts
@@ -84,8 +84,41 @@ export class AnimalService {
 
         let db = new AnimalDb();
         let securityDb = new SecurityDb();
+        
+        router.get("/", async (req,res) => {
+            console.debug(`GET: ${req.url}`);
+            var page = Number.parseInt(req.query["page"] as any || 1); 
+            var limit = Number.parseInt(req.query["limit"] as any || 5);
+            var phrase = req.query["phrase"] as string || '';
 
-        async function routeNewAnimal(req,res) {
+            res.status(200);
+            
+            try {
+                var data = await db.getAnimals(page, limit, phrase);
+                res.json(jsonResponse.createPagination(data,1,page));
+            } catch(error) {
+                res.json(jsonResponse.createError(error));
+            }
+        }); //end routeAnimals
+
+        router.get("/:id", async (req,res) => {
+            console.debug(`GET: ${req.url}`);
+            if (!req.params.id) {
+                    res.status(404);
+                    res.send("HttpGET id not available");
+                    return;
+            }
+            res.status(200);
+            try {
+                var data = await db.getAnimal(req.params.id);
+                res.json(jsonResponse.createData(data));
+            } catch(error) {
+                    console.log(error);
+                    res.json(jsonResponse.createError(error));
+            }
+        }); // end routeGetAnimalWithId
+
+        router.post("/new", jsonBodyParser, async (req,res) => {
             console.debug(`POST: ${req.url}`);
 
             var hashid = req.body.hashid;
@@ -109,9 +142,9 @@ export class AnimalService {
                 console.log(error);
                 res.json(jsonResponse.createError("You do not have access."));
             }
-        } // end routeNewAnimal
-
-        async function routeUpdateAnimalWithId(req,res) {
+        }); // end routeNewAnimal
+        
+        router.post("/:id", jsonBodyParser, async (req,res) => {
             console.debug(`POST [:id] update ${req.url}`);
 
             var id = req.params.id;
@@ -135,46 +168,8 @@ export class AnimalService {
                 console.log(error);
                 res.json(jsonResponse.createError("You do not have access."));
             }
-        } //end routeUpdateAnimalWithId
-
-        async function routeAnimals(req,res) {
-            console.debug(`GET: ${req.url}`);
-            var page = Number.parseInt(req.query["page"] as any || 1); 
-            var limit = Number.parseInt(req.query["limit"] as any || 5);
-            var phrase = req.query["phrase"] as string || '';
-
-            res.status(200);
-            
-            try {
-                var data = await db.getAnimals(page, limit, phrase);
-                res.json(jsonResponse.createPagination(data,1,page));
-            } catch(error) {
-                res.json(jsonResponse.createError(error));
-            }
-        } //end routeAnimals
-
-        async function routeGetAnimalWithId(req,res) {
-            console.debug(`GET: ${req.url}`);
-            if (!req.params.id) {
-                    res.status(404);
-                    res.send("HttpGET id not available");
-                    return;
-            }
-            res.status(200);
-            try {
-                var data = await db.getAnimal(req.params.id);
-                res.json(jsonResponse.createData(data));
-            } catch(error) {
-                    console.log(error);
-                    res.json(jsonResponse.createError(error));
-            }
-        } // end routeGetAnimalWithId
-
-        router.get("/", routeAnimals);
-        router.get("/:id", routeGetAnimalWithId);
-        router.post("/new", jsonBodyParser, routeNewAnimal);
-        router.post("/:id", jsonBodyParser, routeUpdateAnimalWithId);
-        
+        }); //end routeUpdateAnimalWithId       
+                
         app.use("/api/animals", router);        
     } // end publishWebAPI
 }; // end AnimalService class
