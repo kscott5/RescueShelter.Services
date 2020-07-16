bc3dc3d7b083f4ca209f7328d55770d3506ba2dd
Karega K. Scott <kkscott@outlook.com>
2020-07-16 09:33:46 -0500

    default port 3302

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import {SponsorService} from "./sponsorservice";
 import {SecurityService} from "./securityservice";
 
 services.serverName = 'Rescure Shelter Services';
-services.serverPort = 3304;
+services.serverPort = 3302;
 
 services.middleware = [
     new AnimalService().publishWebAPI,
