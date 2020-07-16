7abf4c9df17bfa7c70fba21d54a68f0c7486f5f7
Karega K. Scott <kkscott@outlook.com>
2020-07-16 09:28:35 -0500

    Use with submodule

--- src/server.ts
+++ src/server.ts
@@ -7,75 +7,75 @@ import morgan from "morgan";
 import * as helmet from "helmet";
 import * as path from "path";
 
-import {AriaService} from "./ariaservice";
-import {AnimalService} from "./animalservice";
-import {SponsorService} from "./sponsorservice";
-import {SecurityService} from "./securityservice";
-
-enum LoggerType {
-    Development, Staging, Production
-};
-
-let apiServer = express()
-
 declare let __dirname; // variable initialize by NodeJS Path Module
+
+/**
+ * Name of the express server name
+ */
+export let serverName: string;
+
+/**
+ * Express server listens on port number
+ */
+export let serverPort: Number;
+
+/**
+ * List of function with an Express.Application parameter
+ * 
+ * example:
+ * 
+ *  function serviceName(app: Application): void {}
+ */
+export let middleware: Array<Function>;
+
+/**
+ * List of server hostname with CORS access
+ */
+export let whitelist: Array<string>;
 
 /**
  * Express Http server for the Rescue Shelter App
  */
-export class Server {
-    private port: Number;
+const apiServer = express()
 
-    /**
-     * Starts the express http server
-     * @param  {LoggerType=LoggerType.Development} loggerType
-     * @param  {Number=3302} port
-     * @returns void
-     */
-    start(loggerType: LoggerType = LoggerType.Development, port: Number = 3302) : void {
-        apiServer.use(morgan("dev")); // TODO: Use LoggerType Enum
+// ************************************
+//  Middleware sequential use important
+// ************************************
+apiServer.use(morgan("[development]"));
 
-        var whitelist = [];
-        var corsOptionsDelegate = function (req, callback) {
-            if (whitelist.length == 0 || whitelist.indexOf(req.headers.origin) === 0) {
-                callback(null, true);
-            }
-            else {
-                callback(new Error('Not allowed by CORS'));
-            }
-        };
-        apiServer.use(cors(corsOptionsDelegate));
-        
-        apiServer.use(helmet.contentSecurityPolicy({
-            directives: {
-                defaultSrc: ["'self'"],
-                scriptSrc: ["'self'"],
-                styleSrc: ["'self'"],
-                imgSrc: ["'self'"]
-            }
-        }));
-        
-        let publicPath = path.join(__dirname, '../public');
-        apiServer.use(express.static(publicPath));
-        console.log("wwwroot: " + publicPath);
-        
-        apiServer.get("/apis.html", function(req,res, next) {
-            res.sendFile("apis.html");
-        });
-
-        new AriaService().publishWebAPI(apiServer);
-        new AnimalService().publishWebAPI(apiServer);
-        new SponsorService().publishWebAPI(apiServer);
-        new SecurityService().publishWebAPI(apiServer);
-        
-        apiServer.listen(port,function(){
-            console.log('Rescue Shelter listening on port: '+ port);
-        });
+var corsOptionsDelegate = function (req, callback) {
+    if (whitelist === null || whitelist.length == 0 || whitelist.indexOf(req.headers.origin) === 0) {
+        callback(null, true);
     }
+    else {
+        callback(new Error('Not allowed by CORS'));
+    }
+};
+apiServer.use(cors(corsOptionsDelegate));
+
+apiServer.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'"],
+        styleSrc: ["'self'"],
+        imgSrc: ["'self'"]
+    }
+}));
+
+const publicPath = path.join(__dirname, '../public');
+apiServer.use(express.static(publicPath));
+
+
+export function listener(): void {
+    middleware.forEach((fn) => {
+        fn(apiServer);
+    });
+
+    apiServer.listen(serverPort, () => {
+        var port = (serverPort === null)? 3301: serverPort;
+        var server = (serverName === null)? 'Rescue Shelter': serverName;
+
+        console.log(server + ' listening on port: ' + port);
+        console.log('wwwroot: ' + publicPath);
+    });
 }
-
-export default { Server };
-
-
-let server = new Server();
-server.start();
