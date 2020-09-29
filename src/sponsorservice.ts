954646de85f15951cafa3b9c0f4274c826690cac
Karega K. Scott <kkscott@outlook.com>
2020-09-29 17:33:31 -0500

    Redis structure changes

--- src/sponsorservice.ts
+++ src/sponsorservice.ts
@@ -1,6 +1,5 @@
 import {Application, NextFunction, Request, Response, Router} from "express";
 import * as bodyParser from "body-parser";
-import {RedisClient} from "redis";
 import {CoreServices} from "rescueshelter.core";
 
 let router = Router({ caseSensitive: true, mergeParams: true, strict: true});
@@ -41,43 +40,8 @@ export class SponsorService {
         let jsonResponse = new CoreServices.JsonResponse();
 
         let db = new SponsorDb();
-
-        try {
-            (new RedisClient({host: 'localhost', port: 6379}))?.quit();
-        } catch(error) {
-            console.log('**************These projects are professional entertainment***************')
-            console.log('The following command configures an out of process Redis.io memory cache.');
-            console.log('In process requires Redis.io install in the process of RescueShelter.Reports.');
-            console.log('\n');
-            console.log('docker run -it -p 127.0.0.1:6379:6379 --name redis_dev redis-server --loglevel debug');
-            console.log('\n\n\n');
-            console.log('Terminal/shell access use:> telnet 127.0.0.1 6379');
-            console.log('set \'foo\' \'bar\''); // server response is +OK
-            console.log('get \'foo\''); // server response is $4 bar
-            console.log('quit'); //exit telnet sessions
-        }
     
-        async function validateAccessToken(req: Request, res: Response, next: NextFunction) {
-            // var client: RedisClient;
-            // try {
-            //     client = new RedisClient({host: 'localhost', port: 6379});
-    
-            //     if(client.exists(req.params.id) === true) {
-            //         next();
-            //     }
-            // } catch(error) {            
-            //     res.json(jsonResponse.createError(error));
-            // } finally {
-            //     client?.quit();
-            // }
-
-            next();
-        }
-        
-        app.use(validateAccessToken);
-    
-        router.post("/", jsonBodyParser, async (req,res) => {
-            
+        router.post("/", jsonBodyParser, async (req,res) => {            
             console.debug(`POST: ${req.url}`);
             if(!req.body) {
                 res.status(200);
