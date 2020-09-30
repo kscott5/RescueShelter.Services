ef62f93afb322828590ff6f62813d5ce0c92c61e
Karega K. Scott <kkscott@outlook.com>
2020-09-29 21:56:18 -0500

    Secure all  /api/manage/... expres.routes

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,9 @@ import {AnimalService} from "./animalservice";
 import {SponsorService} from "./sponsorservice";
 import {SecurityService} from "./securityservice";
 
-
+// https://expressjs.com/en/guide/writing-middleware.html
+// The order of middleware insertion is important.
 CoreServer.start('Rescue Shelter Services Server', 3302, [
-    new AnimalService().publishWebAPI,
+    new SecurityService().publishWebAPI, // Secure all routes
     new SponsorService().publishWebAPI,
-    new SecurityService().publishWebAPI], [], "./public");
+    new AnimalService().publishWebAPI], [], "./public");
