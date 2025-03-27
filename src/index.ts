1185f1bb873b30edeb4d55fa6a845ce7507d19db
Karega K. Scott <kkscott@outlook.com>
2025-03-27 15:35:50 -0500

    update wwwroot static path

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,14 @@ import {AnimalService} from "./animalservice";
 import {SponsorService} from "./sponsorservice";
 import {SecurityService} from "./securityservice";
 
+declare let __dirname; // variable initialize by NodeJS Path Module
+
+let path = require("path");
+let staticPath = path.join(__dirname, '/../public');
+
 // https://expressjs.com/en/guide/writing-middleware.html
 // The order of middleware insertion is important.
 CoreServer.start('Rescue Shelter Services Server', 3302, [
     new SecurityService().publishWebAPI, // Secure all routes
     new SponsorService().publishWebAPI,
-    new AnimalService().publishWebAPI], [], "../public");
+    new AnimalService().publishWebAPI], [], staticPath);
