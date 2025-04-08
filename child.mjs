78bb93ef0d1d2c5382ee500c9bf3457a68914145
Karega K. Scott <kkscott@outlook.com>
2025-04-08 15:08:01 -0500

    Did you know. NodeJS .cjs and .mjs are hidden with Typescripts .ts file extensions. A layer of protection or simple another layer.

---
+++ child.mjs
@@ -0,0 +1,14 @@
+import { spawn } from 'node:child_process';
+const ls = spawn('node', ['--inspect', './index.js']);
+
+ls.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
