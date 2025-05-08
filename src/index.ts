608b924dc41b1468a4f5f5085dad1e5af4f80f45
Karega K. Scott <kkscott@outlook.com>
2025-05-07 21:15:37 -0500

    use express route /api/webauth with passport

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import {AriaService} from "./ariaservice";
 import {AnimalService} from "./animalservice";
 import {SponsorService} from "./sponsorservice";
 import {SecurityService} from "./securityservice";
+import { WebAuthnService } from "./webauthnservice";
 
 declare let __dirname; // variable initialize by NodeJS Path Module
 
@@ -12,6 +13,7 @@ let staticPath = path.join(__dirname, '/../public');
 // https://expressjs.com/en/guide/writing-middleware.html
 // The order of middleware insertion is important.
 CoreServer.start('Rescue Shelter Services Server', 3302, [
+    new WebAuthnService().publishWebAPI, // passport
     new SecurityService().publishWebAPI, // Secure all routes
     new SponsorService().publishWebAPI,
     new AnimalService().publishWebAPI], [/* cors */], staticPath);
